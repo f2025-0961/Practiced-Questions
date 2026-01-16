2ed36f20ab5ec70b2496b1a9b2954ad82552b8da
f2025-0961 <f2025-0961@bnu.edu.pk>
2026-01-15 23:35:59 -0700

    string question

--- String/file.cpp
+++ String/file.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 using namespace std;
 
-// int main()
-// {
-//     string s = "i love coding!";
-//     int length = 0;
-//     char ch;
-//     while (ch == '\0')
-//     {
-//         length++;
-//     }
-//     cout << "length of string is: " << length << endl;
+void copyString(string &originalstring, string &copy);
 
-//     return 0;
-// }
+int main()
+{
+    string s = "i love coding!";
+    int length = 0;
+    char ch;
+    while (ch == '\0')
+    {
+        length++;
+    }
+    cout << "length of string is: " << length << endl;
+     string s1 = "hello";
+    string s2 = "";
+    copyString(s1, s2);
+    cout << s1 << endl;
+    cout << s2 << endl;
+
+    return 0;
+}
+
+void copyString(string &originalstring, string &copy)
+{
+    for (int i = 0; i < originalstring.length(); i++)
+    {
+        copy = copy + originalstring[i];
+    }
+}
